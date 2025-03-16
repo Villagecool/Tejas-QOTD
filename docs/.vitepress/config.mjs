290e3df0b31d8cfca906556793353caf04b3ea67
Vlliage <villagercool123@gmail.com>
2025-03-16 10:35:46 -0400

    hopefully finished everything

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   title: "Tejas Quote of the Day",
   description: "QOTD by Tejas",
   base: '/Tejas-QOTD',
+  head: [['link', { rel: 'icon', href: 'https://github.com/Villagecool/Tejas-QOTD/blob/main/docs/favicon.ico?raw=true' }]],
   themeConfig: {
 
     socialLinks: [
